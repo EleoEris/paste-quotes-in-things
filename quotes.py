84bce0577fafbd36a42147767676517edb591742
EleoEris <79694371+EleoEris@users.noreply.github.com>
2021-11-01 17:10:32 +0100

    Random quotes
    The quotes I use are too radical to be shown here lol

---
+++ quotes.py
@@ -0,0 +1,7 @@
+quotes = [
+'We must rapidly begin the shift from a "thing-oriented" society to a "person-oriented" society. When machines and computers, profit motives and property rights are considered more important than people, the giant triplets of racism, materialism, and militarism are incapable of being conquered. -Martin Luther King',
+"Nations do not die from invasion; they die from internal rottenness. -Abraham Lincoln",
+"Let us not seek to satisfy our thirst for freedom by drinking from the cup of bitterness and hatred. -Martin Luther King",
+"Hence I have no mercy or compassion in me for a society that will crush people, and then penalize them for not being able to stand up under the weight. -Malcolm X",
+"We will remember not the words of our enemies, but the silence of our friends. -Martin Luther King",
+]
